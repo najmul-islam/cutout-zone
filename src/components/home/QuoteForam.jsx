a9bc5d474b6f00a276dadd76bfd4361afaa40820
najmul islam <najmulislam519@gmail.com>
2024-04-09 14:16:51 +0600

    fix quoteform desing issue

--- src/components/home/QuoteForam.jsx
+++ src/components/home/QuoteForam.jsx
@@ -1,24 +1,18 @@
 "use client";
 import { useState } from "react";
-import { Input } from "../ui/input";
+import { useForm } from "react-hook-form";
 import Section from "./Section";
-import {
-  Select,
-  SelectContent,
-  SelectGroup,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
+import { MdErrorOutline } from "react-icons/md";
 
 const QuoteForam = () => {
-  const [service, setService] = useState("");
-  const [email, setEmail] = useState("");
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log("service", service);
-    console.log("email", email);
+  const handleFormSubmit = (data) => {
+    console.log(data);
   };
   return (
     <Section className="bg-[#0979E4]">
@@ -32,9 +26,51 @@ const QuoteForam = () => {
         </h6>
       </div>
 
-      <form onSubmit={handleSubmit} className="flex justify-center gap-3">
-        <Select onValueChange={(value) => setService(value)}>
-          <SelectTrigger className="w-[350px] text-white focus:ring-0">
+      <form
+        onSubmit={handleSubmit(handleFormSubmit)}
+        className={`bg:#F1F1F1 m-auto flex max-w-lg flex-col justify-center gap-3 md:max-w-3xl md:flex-row`}
+      >
+        <div className="w-full">
+          <select
+            name="service"
+            id="service"
+            {...register("service", {
+              required: "Service is required",
+            })}
+            className={`w-full rounded border-2 p-3 ${errors?.service ? "border-red-600 focus:border-red-600" : "border-slate-300 focus:border-[#0979E4]"}`}
+          >
+            <option value="">Service Type (required)</option>
+            <option value="Clipping Path and Background Remove">
+              Clipping Path and Background Remove
+            </option>
+            <option value="Shadow Making">Shadow Making</option>
+            <option value="Image Masking">Image Masking</option>
+            <option value="Ghost Mannequin">Ghost Mannequin</option>
+            <option value="Jewelry Retouching">Jewelry Retouching</option>
+            <option value="Photo Retouching">Photo Retouching</option>
+            <option value="Headshot Retouching">Headshot Retouching</option>
+            <option value="Color Correction">Color Correction</option>
+            <option value="Newborn Photo Retouching">
+              Newborn Photo Retouching
+            </option>
+            <option value="Beauty Retouching">Beauty Retouching</option>
+            <option value="E-Commerce Image Solution">
+              E-Commerce Image Solution
+            </option>
+            <option value="Photo Crop and Image Resizing">
+              Photo Crop and Image Resizing
+            </option>
+          </select>
+          <span className="flex items-center gap-1 text-red-600">
+            {errors?.service && (
+              <>
+                <MdErrorOutline /> {errors?.service?.message}
+              </>
+            )}
+          </span>
+        </div>
+        {/* <Select onValueChange={(value) => setService(value)}>
+          <SelectTrigger className=" text-white focus:ring-0">
             <SelectValue placeholder="Service Type (required)" />
           </SelectTrigger>
           <SelectContent>
@@ -67,20 +103,30 @@ const QuoteForam = () => {
               </SelectItem>
             </SelectGroup>
           </SelectContent>
-        </Select>
-        <Input
-          type="email"
-          name="email"
-          id="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          placeholder="Email (required)"
-          className="w-[350px] text-white placeholder:text-white focus:ring-0"
-        />
+        </Select> */}
+        <div className="w-full">
+          <input
+            type="email"
+            name="email"
+            id="email"
+            {...register("email", {
+              required: "Email is required",
+            })}
+            placeholder="Email (required)"
+            className={`w-full rounded border p-3 text-black  placeholder:text-black ${errors?.email ? "border-red-600 focus:border-red-600" : "border-slate-300 focus:border-[#0979E4]"}`}
+          />
+          <span className="flex items-center gap-1 text-red-600">
+            {errors?.email && (
+              <>
+                <MdErrorOutline /> {errors?.email?.message}
+              </>
+            )}
+          </span>
+        </div>
 
         <button
           type="submit"
-          className="rounded bg-white px-4 font-medium hover:shadow-lg"
+          className="h-[50px] rounded bg-black px-8 font-medium  text-white hover:shadow-lg"
         >
           Submit
         </button>
