53d91ab75da73f13fa39064d0b0c609fccc96de3
najmul islam <najmulislam519@gmail.com>
2024-04-20 16:53:17 +0600

    add under construction page

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -5,6 +5,22 @@ export const metadata = {
 };
 
 const BlogPage = () => {
-  return <div>BlogPage</div>;
+  return (
+    <div class="bg-gray-100">
+      <div class=" flex min-h-dvh flex-col items-center justify-center">
+        <img
+          src="https://www.svgrepo.com/show/426192/cogs-settings.svg"
+          alt="Logo"
+          class="mb-8 h-40"
+        />
+        <h1 class="mb-4 text-center text-4xl font-bold text-gray-700 md:text-5xl lg:text-6xl">
+          Blog page is under maintenance
+        </h1>
+        <p class="mb-8 text-center text-lg text-gray-500 md:text-xl lg:text-2xl">
+          We're working hard to improve the user experience. Stay tuned!
+        </p>
+      </div>
+    </div>
+  );
 };
 export default BlogPage;
